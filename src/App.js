89261b1c1cf4d84307e4499f6f003afdc4ae6e06
Farchi <farcas683@gmail.com>
2023-02-28 22:10:50 +0200

    login-form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import {IonIcon} from '@ionic/react';
+import { mailOutline, lockClosedOutline } from 'ionicons/icons';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section>
+      <div className="form-box">
+        <div className="form-value">
+          <form action="">
+            <h2>Login</h2>
+            <div className="inputbox">
+              <IonIcon icon={mailOutline}></IonIcon>
+              <input type="email" required></input>
+              <label htmlFor="">Email</label>
+            </div>
+            <div className="inputbox">
+              <IonIcon icon={lockClosedOutline}></IonIcon>
+              <input type="password" required></input>
+              <label htmlFor="">Password</label>
+            </div>
+            <div className="forget">
+              <input type="checkbox" id="remember-me"/>
+              <label htmlFor="remember-me">Remember Me</label>
+              <a href='/#'>Forget Password</a>
+            </div>
+            <button>Log In</button>
+            <div className="register">
+              <p>Don't have an account <a href='/#' title='Register'>Sign up</a></p>
+            </div>
+          </form>
+        </div>
+      </div>
+    </section>
   );
 }
 
